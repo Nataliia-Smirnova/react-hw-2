1e8b46bfafc2bca3d40f07baceea3ac5ed0fcedd
Nataliia <n3v3st4@gmail.com>
2020-09-05 20:35:21 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
+
+ReactDOM.render(
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById('root'),
+);
