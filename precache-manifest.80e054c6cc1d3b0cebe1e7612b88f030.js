8f817fdb0dcc0f8ead3b515e5f27f027543775fc
Nataliia <n3v3st4@gmail.com>
2020-09-07 01:36:53 +0300

    Updates

---
+++ precache-manifest.80e054c6cc1d3b0cebe1e7612b88f030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3511b89d56c82fe553ef4d6d4ff78120",
+    "url": "/react-hw-2/index.html"
+  },
+  {
+    "revision": "51daf8f679feebb3b68d",
+    "url": "/react-hw-2/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "51daf8f679feebb3b68d",
+    "url": "/react-hw-2/static/js/2.d9aa85ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-2/static/js/2.d9aa85ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b709410745ef5ec3b3c7",
+    "url": "/react-hw-2/static/js/main.120cec8a.chunk.js"
+  },
+  {
+    "revision": "96003b259c6a84e9b86f",
+    "url": "/react-hw-2/static/js/runtime-main.418d12e2.js"
+  }
+]);
